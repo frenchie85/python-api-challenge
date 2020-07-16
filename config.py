6c809e677027c93d12be4a45a2b986d41d1d64fb
Sylvain David <youremail@gmail.com>
2020-07-15 21:11:39 -0400

    gitignore

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "AVAILABLE UPON REQUEST"
+weather_api_key = "5f1ac7497456a5345758edf63daad89e"
 
 # Google API Key
-g_key = "AVAILABLE UPON REQUEST"
+g_key = "AIzaSyCf79eUsDj-LCFKI2Yb0E3rSt6ve9TTd9E"
